b0c794491a0eb7816128886c8dd42825e00d4f72
ParkHawk <park.hawk@gmail.com>
2017-07-19 00:42:54 -0400

    completed placeholder images and also made the boxes organize inner content much better

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ body.appendChild(document.createElement("hr"));
 body.appendChild(div);
 
 
+
 document.getElementById("button").addEventListener("click", function() {
   div.innerHTML = ``;
   fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${input.value}`)
@@ -20,20 +21,16 @@ document.getElementById("button").addEventListener("click", function() {
         console.log(data);
 
         for (var i = 0; i < data.results.length; i++) {
-          // // console.log("kill it");
-          // // console.log(data.results[i].thumbnail);
-          // if (data.results[i].thumbnail === null || data.results[i].thumbnail === undefined) {
-          //   data.results[i].thumbnail = "http://via.placeholder.com/150x150"
-          //   // img.src = "http://via.placeholder.com/150x150";
-          //   console.log("if statement");
-          //   // break;
-          // } else {
-          //   console.log("else statement");
-          //   data.results[i].thumbnail = data.results[i].thumbnail;
-          // }
+          console.log(data.results[i].thumbnail);
           let section = document.createElement("section");
           div.appendChild(section);
-          section.innerHTML = `<h2>${data.results[i].title}</h2><img src = ${data.results[i].thumbnail}><a href = ${data.results[i].href} >Recipe</a>`
+          section.className = "fullBox"
+
+          if (data.results[i].thumbnail === "") {
+            data.results[i].thumbnail = "http://via.placeholder.com/150x150"
+            console.log("if statement");
+          }
+          section.innerHTML = `<h2>${data.results[i].title}</h2><section id = "innerBox"><img src = ${data.results[i].thumbnail}><a href = ${data.results[i].href} >Recipe</a></section>`
         }
       })
     })
