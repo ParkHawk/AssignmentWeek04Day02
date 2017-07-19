62a2a1c4332d82e4a40ae51610992f2cf303e3c3
ParkHawk <park.hawk@gmail.com>
2017-07-19 00:08:28 -0400

    Everything is working and looks beautiful except filler images

---
+++ main.js
@@ -0,0 +1,40 @@
+let body = document.querySelector("body");
+let input = document.createElement("input");
+let button = document.createElement("button");
+button.id = "button";
+input.placeholder = "Food";
+button.innerHTML = `Search`;
+body.appendChild(input);
+body.appendChild(button);
+let searchTerm = document.getElementById('searchTerm');
+let div = document.createElement("div");
+body.appendChild(document.createElement("hr"));
+body.appendChild(div);
+
+
+document.getElementById("button").addEventListener("click", function() {
+  div.innerHTML = ``;
+  fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${input.value}`)
+    .then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+
+        for (var i = 0; i < data.results.length; i++) {
+          // // console.log("kill it");
+          // // console.log(data.results[i].thumbnail);
+          // if (data.results[i].thumbnail === null || data.results[i].thumbnail === undefined) {
+          //   data.results[i].thumbnail = "http://via.placeholder.com/150x150"
+          //   // img.src = "http://via.placeholder.com/150x150";
+          //   console.log("if statement");
+          //   // break;
+          // } else {
+          //   console.log("else statement");
+          //   data.results[i].thumbnail = data.results[i].thumbnail;
+          // }
+          let section = document.createElement("section");
+          div.appendChild(section);
+          section.innerHTML = `<h2>${data.results[i].title}</h2><img src = ${data.results[i].thumbnail}><a href = ${data.results[i].href} >Recipe</a>`
+        }
+      })
+    })
+});
